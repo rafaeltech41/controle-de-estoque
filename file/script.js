ded232555fce7537b274871817320c6e27d7812c
rafaeltech41 <rafaelmoreiralopes41@gmail.com>
2025-03-12 14:20:15 -0300

    feat: first commit

---
+++ file/script.js
@@ -0,0 +1,11 @@
+class Produto{
+
+    constructor(){
+        this.id = 0;
+        this.nomeProduto = "";
+        this.value = 0;
+    }
+    adicionar(){alert('teste')}
+    excluir(){alert('item deletado')}
+}
+var produto = new Produto();
